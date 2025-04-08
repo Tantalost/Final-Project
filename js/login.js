20cb388154caac85a1701667a54a5bb9da03221b
Lol <133163245+Tantalost@users.noreply.github.com>
2025-04-08 15:34:56 +0800

    added login redirections

---
+++ js/login.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const loginForm = document.getElementById('loginForm');
+    const logoutBtn = document.getElementById('logoutbtn');
+
+    if (localStorage.getItem('isLoggedIn') === 'true' &&)
+})
