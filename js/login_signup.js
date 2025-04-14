ec002a05f44816f7642bd8c8e13180f1ff58f29b
Lol <133163245+Tantalost@users.noreply.github.com>
2025-04-14 14:14:16 +0800

    functional buttons

---
+++ js/login_signup.js
@@ -0,0 +1,17 @@
+// Tempo Sign Up and Log in no database yet
+document.addEventListener('DOMContentLoaded', function () {
+    const goToAdminBtn = document.getElementById('gotoadmin');
+    const goToMemberBtn = document.getElementById('gotomember');
+
+    if (goToAdminBtn) {
+        goToAdminBtn.addEventListener('click', function () {
+            window.location.href = '/HTML/admindash.html';
+        });
+    }
+
+    if (goToMemberBtn) {
+        goToMemberBtn.addEventListener('click', function () {
+            window.location.href = '/HTML/Member-Homepage.html';
+        });
+    }
+});
